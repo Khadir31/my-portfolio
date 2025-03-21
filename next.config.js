cd157cce2d4ecd6b0108af166b7f97f427644ab8
Khadir31 <akhadir923@gmail.com>
2025-03-21 13:05:24 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,13 +5,14 @@ const nextConfig = {
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-      },      
+      },
       {
         protocol: "https",
         hostname: "assets.aceternity.com",
       },
     ],
   },
+  output: "standalone", // Helps Vercel deploy correctly
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
