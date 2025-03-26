2281b35b632dbef360054f288dd8f3c925fe3da9
Khadir31 <akhadir923@gmail.com>
2025-03-26 14:41:54 +0800

    8

--- components/cards/BlogCard.tsx
+++ components/cards/BlogCard.tsx
@@ -12,12 +12,12 @@ export const BlogCard: React.FC<BlogCardProps> = ({
   return (
     <div className="w-full flex flex-col gap-4">
       <p className="text-small text-default-500 text-left font-bold text-lg">
-      September 16, 2024
+        {date}
       </p>
       <p className="text-xl lg:text-2xl md:text-lg text-left font-bold">
-      My Journey into Web Development
+        {title}
       </p>
-      <p className="text-md lg:text-xl md:text-lg text-left"> Sharing my experience of learning frontend and backend technologies, working on exciting projects, and building a strong portfolio.</p>
+      <p className="text-md lg:text-xl md:text-lg text-left">{description}</p>
     </div>
   );
 };
